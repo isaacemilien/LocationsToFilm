e0dab326a884307d1a376d01a19f7a26c59bb27d
Pavan Sahadevan <93722550+PavanSahadevan46@users.noreply.github.com>
2022-05-20 11:12:28 +0100

    Add files via upload

--- header.php
+++ header.php
@@ -8,21 +8,23 @@
     <title>Document</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+    <script src="https://kit.fontawesome.com/801a4238f4.js" crossorigin="anonymous"></script>
     <link rel="stylesheet" href="styles.css">
     <!-- font-awesome library -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css">
     <script src="https://code.jquery.com/jquery-1.10.2.js"></script>
+    <script src="https://code.iconify.design/2/2.2.1/iconify.min.js"></script>
   </head>
   <body>
 
-
+<!-- <i class="fa-solid fa-video"></i> -->
 <!-- Navbar -->
 <div class="px-3 py-2 bg-dark text-white">
 
       <div class="container bg-dark">
         <header class="d-flex flex-wrap justify-content-center">
           <a href="index.php" class="d-flex align-items-center mb-3 mb-md-0 me-md-auto text-dark text-decoration-none">
-            <span class="fs-4 text-light">Locations to Film</span>
+            <span class="fs-4 text-light"><span class="iconify" data-icon="oi:aperture" data-width="35" data-height="35"></span>&nbsp;&nbsp;Locations to Film</span>
           </a>
     
           <ul class="nav nav-pills text-light">
