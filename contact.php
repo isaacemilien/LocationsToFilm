f81cc51fe5144f4fb7ece870089da8bb4befb8b0
Isaac Emilien <isaac.emilien@gmail.com>
2022-05-20 14:47:46 +0100

    Reformatted contact page

--- contact.php
+++ contact.php
@@ -10,64 +10,75 @@
         </div>
       </div>
     <!-- Office Location cards -->
-    <div class="row">
-      <div class="col-lg-4">  
-        <h2>Azerbaijan</h2>
-        <h5>Address:</h5>
-        <p>Ziya Bunyadov 1965</br>
-          Chinar Park</br>
-          Baku AZ1000</br>
-          Azerbaijan</br>
-          <h5>Telephone Number:</h5>
-          <a href="tel:+994 50 216 32 65">+994 50 216 32 65</a>
-        </p>
-      </div><!-- /.col-lg-4 -->
-      <div class="col-lg-4">
-        <h2>United Kingdom</h2>
-        <h5>Address:</h5>
-        <p>Commercial House</br>
-        52 Perrymount Road</br>
-        Haywards Heath</br>
-        West Sussex</br>
-        RH16 3DT</br>
-          <h5>Telephone Number:</h5>
-          <a href="tel:+44 1444 440957">+44 1444 440957</a>
-        </p>
-      </div><!-- /.col-lg-4 -->
-      <div class="col-lg-4">
-
-        <h2>USA</h2>
-        <h5>Address:</h5>
-        <p>The Water Garden</br>
-          2425 Olympic Blvd.</br>
-          Santa Monica</br>
-          90404</br>
-          CA</br>
-          <h5>Telephone Number:</h5>
-          <a href="tel:+1 424 295 9760">+1 424 295 9760</a>
+    <div class="row justify-content-center m-5">
+      <div class="col-lg-4 d-flex justify-content-center">
+        <div>
+          <h2>Azerbaijan</h2>
+          <h5>Address:</h5>
+          <p>Ziya Bunyadov 1965</br>
+            Chinar Park</br>
+            Baku AZ1000</br>
+            Azerbaijan</br>
+            <h5>Telephone Number:</h5>
+            <a href="tel:+994 50 216 32 65">+994 50 216 32 65</a>
           </p>
-      </div><!-- /.col-lg-4 -->
-      <div class="col-lg-4">
-
-        <h2>UAE</h2>
-        <h5>Address:</h5>
-        <p>Business Centre RAKEZ,</br>
-          Dubai</br>
-          United Arab Emirates</br>
-          <h5>Telephone Number:</h5>
-          <a href="tel:+971 52 375 80 85">+971 52 375 80 85</a>
+        </div>
+      </div>
+      <div class="col-lg-4 d-flex justify-content-center">
+        <div>
+          <h2>United Kingdom</h2>
+          <h5>Address:</h5>
+          <p>Commercial House</br>
+            52 Perrymount Road</br>
+            Haywards Heath</br>
+            West Sussex</br>
+            RH16 3DT</br>
+            <h5>Telephone Number:</h5>
+            <a href="tel:+44 1444 440957">+44 1444 440957</a>
           </p>
-      </div><!-- /.col-lg-4 -->
-      <div class="col-lg-4">
+        </div>
+      </div>
+      <div class="col-lg-4 d-flex justify-content-center">
+        <div>
+          <h2>USA</h2>
+          <h5>Address:</h5>
+          <p>The Water Garden</br>
+            2425 Olympic Blvd.</br>
+            Santa Monica</br>
+            90404</br>
+            CA</br>
+            <h5>Telephone Number:</h5>
+            <a href="tel:+1 424 295 9760">+1 424 295 9760</a>
+          </p>
+        </div>
+      </div>
+      
+    </div>
 
-        <h2>Poland</h2>
-        <h5>Address:</h5>
-        <p>Aleje Jerozolimskie 214</br>
+    <div class="row justify-content-center m-5">
+      <div class="col-lg-4 d-flex justify-content-center">
+        <div>
+          <h2>UAE</h2>
+          <h5>Address:</h5>
+          <p>Business Centre RAKEZ,</br>
+            Dubai</br>
+            United Arab Emirates</br>
+            <h5>Telephone Number:</h5>
+            <a href="tel:+971 52 375 80 85">+971 52 375 80 85</a>
+          </p>
+        </div>
+      </div>
+      <div class="col-lg-4 d-flex justify-content-center">
+        <div>
+          <h2>Poland</h2>
+          <h5>Address:</h5>
+          <p>Aleje Jerozolimskie 214</br>
           Warszawa</br>
           <h5>Telephone Number:</h5>
           <a href="tel:+48 503 535 881">+48 503 535 881</a>
           </p>
-      </div><!-- /.col-lg-4 -->
-    </div><!-- /.row -->
+        </div>
+      </div>
+    </div>
   </div>
 <?php include "footer.php"; ?>
