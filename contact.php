7f7fb6bd1119876c4d085030f057eec51ade01d6
Isaac Emilien <isaac.emilien@gmail.com>
2022-05-31 14:00:40 +0100

    Formatted new contacts page layout

--- contact.php
+++ contact.php
@@ -1,84 +1,104 @@
 <?php include "header.php"; ?>
-  <div class="container-fluid">
-    <!-- Background -->
-    <div class="row bg h-100 d-lg-flex justify-content-lg-start justify-content-center" style="background-image: url('imgs/contact.jpg')">
-        <div class="col-lg-6 col-12 col-10 my-5 bg-dark p-5 m-5" style="--bs-bg-opacity: .5;">
-          <h1 class="fw-bold text-light" >Contact Us</h1>
-          <p class="text-light d-none d-lg-block">
-            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Aliquam sequi vitae cupiditate, eum temporibus voluptatem beatae architecto atque, vel accusantium voluptas quam nemo neque quia deserunt. Quis obcaecati odio esse.
-          </p>
-        </div>
+  <!-- Background -->
+  <div class="row bg h-100 d-lg-flex justify-content-lg-start justify-content-center" style="background-image: url('imgs/contact.jpg')">
+    <div class="col-lg-6 col-12 col-10 my-5 bg-dark p-5 m-5" style="--bs-bg-opacity: .5;">
+      <h1 class="fw-bold text-light" >Contact Us</h1>
+      <p class="text-light d-none d-lg-block">
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Aliquam sequi vitae cupiditate, eum temporibus voluptatem beatae architecto atque, vel accusantium voluptas quam nemo neque quia deserunt. Quis obcaecati odio esse.
+      </p>
+    </div>
+  </div>
+  <!-- Contact content -->
+  <!-- Azerbaijan office -->
+  <div class="row justify-content-center">
+    <div class="col-lg-5 my-5 p-5 m-5 d-flex justify-content-center">
+      <div>
+        <h2>Azerbaijan</h2>
+        <h3>Address</h5>
+        <p class="fs-5">Ziya Bunyadov 1965</br>
+          Chinar Park</br>
+          Baku AZ1000</br>
+          Azerbaijan</br>
+          <h3>Telephone Number</h3>
+          <a class="fs-5" href="tel:+994 50 216 32 65">+994 50 216 32 65</a>
+        </p>
       </div>
-    <!-- Office Location cards -->
-    <div class="row justify-content-center m-5">
-      <div class="col-lg-4 col-10 d-flex justify-content-center">
-        <div>
-          <h2>Azerbaijan</h2>
-          <h5>Address:</h5>
-          <p>Ziya Bunyadov 1965</br>
-            Chinar Park</br>
-            Baku AZ1000</br>
-            Azerbaijan</br>
-            <h5>Telephone Number:</h5>
-            <a href="tel:+994 50 216 32 65">+994 50 216 32 65</a>
-          </p>
-        </div>
-      </div>
-      <div class="col-lg-4 d-flex justify-content-center">
-        <div>
+    </div>
+    <div class="col-lg-5 mt-5 ms-5 pe-4 d-none d-lg-flex justify-content-center">
+      <span class="iconify" data-icon="fa6-solid:people-roof" data-width="300" data-height="300"></span>
+    </div>
+  </div>
+  <!-- UK office -->
+  <div class="row bg h-100 d-lg-flex justify-content-lg-end justify-content-center" style="background-image: url('imgs/urban_01.jpg')">
+    <div class="col-lg-6 col-12 my-5 p-5 m-5 text-light d-flex justify-content-center">
+      <div class="bg-dark p-4" style="--bs-bg-opacity: .5;">
           <h2>United Kingdom</h2>
-          <h5>Address:</h5>
-          <p>Commercial House</br>
+          <h3>Address</h3>
+          <p class = "fs-5">Commercial House</br>
             52 Perrymount Road</br>
             Haywards Heath</br>
             West Sussex</br>
-            RH16 3DT</br>
-            <h5>Telephone Number:</h5>
-            <a href="tel:+44 1444 440957">+44 1444 440957</a>
+            RH16 3DT  </br>
+            <h3>Telephone Number</h3>
+            <a class="fs-5" href="tel:+44 1444 440957">+44 1444 440957</a>
           </p>
-        </div>
       </div>
-      <div class="col-lg-4 d-flex justify-content-center">
-        <div>
-          <h2>USA</h2>
-          <h5>Address:</h5>
-          <p>The Water Garden</br>
+    </div>
+  </div>
+
+  <!-- USA office -->
+  <div class="row justify-content-center">
+    <div class="col-lg-5 my-5 p-5 m-5 d-flex justify-content-center">
+      <div>
+        <h2>USA</h2>
+        <h3>Address</h5>
+        <p class="fs-5">The Water Garden</br>
             2425 Olympic Blvd.</br>
             Santa Monica</br>
             90404</br>
             CA</br>
-            <h5>Telephone Number:</h5>
-            <a href="tel:+1 424 295 9760">+1 424 295 9760</a>
-          </p>
-        </div>
+          <h3>Telephone Number</h3>
+          <a class="fs-5" href="tel:+1 424 295 9760">+1 424 295 9760</a>
+        </p>
       </div>
-      
     </div>
+    <div class="col-lg-5 mt-5 ms-5 pe-4 d-none d-lg-flex justify-content-center">
+      <span class="iconify" data-icon="fa6-solid:people-roof" data-width="300" data-height="300"></span>
+    </div>
+  </div>
 
-    <div class="row justify-content-center m-5">
-      <div class="col-lg-4 d-flex justify-content-center">
-        <div>
+  <!-- Azerbaijan office -->
+  <div class="row bg h-100 d-lg-flex justify-content-lg-end justify-content-center" style="background-image: url('imgs/urban_02.jpg')">
+    <div class="col-lg-6 col-12 my-5 p-5 m-5 text-light d-flex justify-content-center">
+      <div class="bg-dark p-4" style="--bs-bg-opacity: .5;">
           <h2>UAE</h2>
-          <h5>Address:</h5>
-          <p>Business Centre RAKEZ,</br>
+          <h3>Address</h3>
+          <p class = "fs-5">Business Centre RAKEZ,</br>
             Dubai</br>
             United Arab Emirates</br>
-            <h5>Telephone Number:</h5>
-            <a href="tel:+971 52 375 80 85">+971 52 375 80 85</a>
+            <h3>Telephone Number:</h3>
+            <a class="fs-5" href="tel:+971 52 375 80 85">+971 52 375 80 85</a>
           </p>
-        </div>
       </div>
-      <div class="col-lg-4 d-flex justify-content-center">
-        <div>
-          <h2>Poland</h2>
-          <h5>Address:</h5>
+    </div>
+  </div>
+
+  <!-- Poland office -->
+  <div class="row justify-content-center">
+    <div class="col-lg-5 my-5 p-5 m-5 d-flex justify-content-center">
+      <div>
+        <h2>Azerbaijan</h2>
+        <h3>Address</h5>
+        <p class="fs-5"></h5>
           <p>Aleje Jerozolimskie 214</br>
           Warszawa</br>
-          <h5>Telephone Number:</h5>
-          <a href="tel:+48 503 535 881">+48 503 535 881</a>
-          </p>
-        </div>
+          <h3>Telephone Number</h5>
+          <a class="fs-5" href="tel:+48 503 535 881">+48 503 535 881</a>
+        </p>
       </div>
+    </div>
+    <div class="col-lg-5 mt-5 ms-5 pe-4 d-none d-lg-flex justify-content-center">
+      <span class="iconify" data-icon="fa6-solid:people-roof" data-width="300" data-height="300"></span>
     </div>
   </div>
 <?php include "footer.php"; ?>
