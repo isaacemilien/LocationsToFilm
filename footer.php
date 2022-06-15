49ce7d38ab992b5e9d3ab01b41da35c78050623c
PavanSahadevan46 <pavansahadevan46@gmail.com>
2022-06-15 10:41:15 +0100

    Footer update, removed underscores upon the footer thing hyperpontifcation spotanteious poggers malding

--- footer.php
+++ footer.php
@@ -1,86 +1,100 @@
 <!-- Footer -->
 <footer class="text-center text-lg-start text-white bg-dark" style="background-color: #1c2331">
-      <!-- Section: Social media -->
-      <section class="d-flex justify-content-between p-4" style="background-color: #6351ce">
-      </section>
-      <!-- Section: Social media -->
-      <!-- Section: Links  -->
-      <section class="">
-        <div class="container text-center text-md-start mt-5">
-          <!-- Grid row -->
-          <div class="row mt-3">
-            <!-- Grid column -->
-            <div class="col-md-3 col-lg-4 col-xl-3 mx-auto mb-4">
-              <!-- Content -->
-              <span class="fs-4 text-light"><span class="iconify" data-icon="oi:aperture" data-width="35" data-height="35"></span>&nbsp;&nbsp;Locations to Film</span>
-              <hr class="mb-4 mt-0 d-inline-block mx-auto" />
-              <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptates temporibus amet explicabo aliquid dolore quae velit sunt quibusdam. Modi, beatae.</p>
-            </div>
-            <!-- Grid column -->
-            <!-- Grid column -->
-            <div class="col-md-2 col-lg-2 col-xl-2 mx-auto mb-4">
-              <!-- Links -->
-              <h6 class="text-uppercase fw-bold">Quick links to Products</h6>
-              <hr class="mb-4 mt-0 d-inline-block mx-auto" style="width: 60px; background-color: #7c4dff; height: 2px" />
-              <p>
-                <a href="#!" class="text-white text-decoration-none">Cameras</a>
-              </p>
-              <p>
-                <a href="#!" class="text-white text-decoration-none"> Lighting Equipment</a>
-              </p>
-              <p>
-                <a href="#!" class="text-white text-decoration-none">Permits</a>
-              </p>
-              <p>
-                <a href="#!" class="text-white text-decoration-none">Merchandise</a>
-              </p>
-            </div>
-            <!-- Grid column -->
-            <!-- Grid column -->
-            <div class="col-md-3 col-lg-2 col-xl-2 mx-auto mb-4">
-              <!-- Links -->
-              <h6 class="text-uppercase fw-bold">Useful links</h6>
-              <hr class="mb-4 mt-0 d-inline-block mx-auto" style="width: 60px; background-color: #7c4dff; height: 2px" />
-              <p>
-                <a href="#!" class="text-white text-decoration-none">Services</a>
-              </p>
-              <p>
-                <a href="#!" class="text-white text-decoration-none">Work with us</a>
-              </p>
-              <p>
-                <a href="#!" class="text-white text-decoration-none">Privacy Policy</a>
-              </p>
-              <p>
-                <a href="#!" class="text-white text-decoration-none">Help</a>
-              </p>
-            </div>
-            <!-- Grid column -->
-            <!-- Grid column -->
-            <div class="col-md-4 col-lg-3 col-xl-3 mx-auto mb-md-0 mb-4">
-              <!-- Links -->
-              <h6 class="text-uppercase fw-bold">Contact</h6>
-              <hr class="mb-4 mt-0 d-inline-block mx-auto" style="width: 60px; background-color: #7c4dff; height: 2px" />
-              <p>
-                <i class="fas fa-home mr-3"></i> Ziya Bunyadov 1965, Chinar Park, Baku, AZ1000, Azerbaijan
-              </p>
-              <p>
-                <i class="fas fa-envelope mr-3 "><a href = "mailto: Enquiries@LocationsToFilm.com" class="text-light text-decoration-none"></i> Enquiries@LocationsToFilm.com</a>
-              </p> 
-              <p>
-                <i class="fas fa-phone mr-3"></i> +994 50 216 32 65
-              </p>
-            </div>
-            <!-- Grid column -->
-          </div>
-          <!-- Grid row -->
+  <!-- Section: Social media -->
+  <section class="d-flex justify-content-between p-4" style="background-color: #6351ce">
+    <!-- Left -->
+    <div class="me-5">
+      <span>Get connected with us on social networks:</span>
+    </div>
+    <!-- Left -->
+    <!-- Right -->
+    <div>
+      <i class="fab fa-facebook-f text-white me-4"></i>
+      <i class="fab fa-twitter text-white me-4"></i>
+      <i class="fab fa-google text-white me-4"></i>
+      <i class="fab fa-instagram text-white me-4"></i>
+    </div>
+    <!-- Right -->
+  </section>
+  <!-- Section: Social media -->
+  <!-- Section: Links  -->
+  <section class="">
+    <div class="container text-center text-md-start mt-5">
+      <!-- Grid row -->
+      <div class="row mt-3">
+        <!-- Grid column -->
+        <div class="col-md-3 col-lg-4 col-xl-3 mx-auto mb-4">
+          <!-- Content -->
+          <span class="fs-4 text-light"><span class="iconify" data-icon="oi:aperture" data-width="35" data-height="35"></span>&nbsp;&nbsp;Locations to Film</span>
+          <hr class="mb-4 mt-0 d-inline-block mx-auto" />
+          <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptates temporibus amet explicabo aliquid dolore quae velit sunt quibusdam. Modi, beatae.</p>
         </div>
-      </section>
-      <!-- Section: Links  -->
-      <!-- Copyright -->
-      <div class="text-center p-3" style="background-color: rgba(0, 0, 0, 0.2)"> © 2022 Copyright: Locations To Film </div>
-      <!-- Copyright -->
-    </footer>
-    <!-- Footer end -->
+        <!-- Grid column -->
+        <!-- Grid column -->
+        <div class="col-md-2 col-lg-2 col-xl-2 mx-auto mb-4">
+          <!-- Links -->
+          <h6 class="text-uppercase fw-bold">Quick links to Products</h6>
+          <hr class="mb-4 mt-0 d-inline-block mx-auto" style="width: 60px; background-color: #7c4dff; height: 2px" />
+          <p>
+            <a href="#!" class="text-white text-decoration-none">Cameras</a>
+          </p>
+          <p>
+            <a href="#!" class="text-white text-decoration-none"> Lighting Equipment</a>
+          </p>
+          <p>
+            <a href="#!" class="text-white text-decoration-none">Permits</a>
+          </p>
+          <p>
+            <a href="#!" class="text-white text-decoration-none">Merchandise</a>
+          </p>
+        </div>
+        <!-- Grid column -->
+        <!-- Grid column -->
+        <div class="col-md-3 col-lg-2 col-xl-2 mx-auto mb-4">
+          <!-- Links -->
+          <h6 class="text-uppercase fw-bold">Useful links</h6>
+          <hr class="mb-4 mt-0 d-inline-block mx-auto" style="width: 60px; background-color: #7c4dff; height: 2px" />
+          <p>
+            <a href="#!" class="text-white text-decoration-none">Services</a>
+          </p>
+          <p>
+            <a href="#!" class="text-white text-decoration-none">Work with us</a>
+          </p>
+          <p>
+            <a href="#!" class="text-white text-decoration-none">Privacy Policy</a>
+          </p>
+          <p>
+            <a href="#!" class="text-white text-decoration-none">Help</a>
+          </p>
+        </div>
+        <!-- Grid column -->
+        <!-- Grid column -->
+        <div class="col-md-4 col-lg-3 col-xl-3 mx-auto mb-md-0 mb-4">
+          <!-- Links -->
+          <h6 class="text-uppercase fw-bold">Contact</h6>
+          <hr class="mb-4 mt-0 d-inline-block mx-auto" style="width: 60px; background-color: #7c4dff; height: 2px" />
+          <p>
+            <i class="fas fa-home mr-3"></i> Ziya Bunyadov 1965, Chinar Park, Baku, AZ1000, Azerbaijan
+          </p>
+          <p>
+            <i class="fas fa-envelope mr-3 "><a href="mailto: Enquiries@LocationsToFilm.com" class="text-light text-decoration-none"></i> Enquiries@LocationsToFilm.com</a>
+          </p>
+          <p>
+            <i class="fas fa-phone mr-3"></i> +994 50 216 32 65
+          </p>
+        </div>
+        <!-- Grid column -->
+      </div>
+      <!-- Grid row -->
+    </div>
+  </section>
+  <!-- Section: Links  -->
+  <!-- Copyright -->
+  <div class="text-center p-3" style="background-color: rgba(0, 0, 0, 0.2)"> © 2022 Copyright: Locations To Film </div>
+  <!-- Copyright -->
+</footer>
+<!-- Footer end -->
 
-    </body>
+</body>
+
 </html>
